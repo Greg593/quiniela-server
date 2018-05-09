2c2ad505b7d4619391e85ed3ba5940dbb585dcc2
Víctor Morales <vic.mor.593@gmail.com>
2018-05-09 02:07:01 -0600

    Servidor
    Se crea servidor MongoDB para la quiniela

---
+++ server/server.js
@@ -0,0 +1,56 @@
+// Set up
+var express  = require('express');
+var app      = express();                               // create our app w/ express
+var mongoose = require('mongoose');                     // mongoose for mongodb
+var morgan = require('morgan');             // log requests to the console (express4)
+var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
+var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+var cors = require('cors');
+ 
+// Configuration
+mongoose.connect('mongodb://localhost/quiniela');
+ 
+app.use(morgan('dev'));                                         // log every request to the console
+app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(methodOverride());
+app.use(cors());
+ 
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+});
+ 
+// Models
+var Ganadores = mongoose.model('ganadores', {
+    anio: Number,
+    sede: String,
+    primer_lugar: String,
+    segundo_lugar: String,
+    tercer_lugar: String
+});
+ 
+// Routes
+ 
+    // Get Ganadores
+    app.get('/api/ganadores', function(req, res) {
+ 
+        console.log("fetching ganadores");
+ 
+        // use mongoose to get all Ganadores in the database
+        Ganadores.find(function(err, ganadores) {
+ 
+            // if there is an error retrieving, send the error. nothing after res.send(err) will execute
+            if (err)
+                res.send(err)
+ 
+            res.json(ganadores); // return all Ganadores in JSON format
+        });
+    });
+ 
+// listen (start app with node server.js) ======================================
+app.listen(8080);
+console.log("App listening on port 8080");
